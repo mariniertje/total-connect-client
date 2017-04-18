427c24c54a87213079cb6864a55881d39dcd7591
mariniertje <mariniertje@gmail.com>
2017-04-18 11:44:49 -0700

    Update setup.py
    Configured setup.py for ADT Pulse Client

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 #from distutils.core import setup
 from setuptools import setup, find_packages
 setup(
-  name = 'total_connect_client',
-  py_modules = ['total_connect_client'],
-  version = '0.07',
-  description = 'Interact with Total Connect 2 alarm systems',
-  author = 'Craig J. Ward',
-  author_email = 'ward.craig.j@gmail.com',
-  url = 'https://github.com/wardcraigj/total-connect-client',
-  download_url = 'https://github.com/wardcraigj/total-connect-client',
-  keywords = ['alarm','TotalConnect'],
+  name = 'adt_pulse_client',
+  py_modules = ['adt_pulse_client'],
+  version = '0.1',
+  description = 'Interact with ADT Pulse alarm systems',
+  author = 'Jeroen A. Goddijn',
+  author_email = 'mariniertje@gmail.com',
+  url = 'https://github.com/mariniertje/adt_pulse_client',
+  download_url = 'https://github.com/mariniertje/adt_pulse_client',
+  keywords = ['alarm','ADTPulse'],
   package_data = {'': ['data/*.json']},
   requires = ['zeep', 'pprint', 'logging', 'logging.handlers', 'sys'],
   install_requires = ['zeep'],
 #  packages=find_packages(exclude=['tests', 'tests.*']),
-  packages=['total_connect_client'],
+  packages=['adt_pulse_client'],
   include_package_data=True, # use MANIFEST.in during install
   zip_safe=False
 )
